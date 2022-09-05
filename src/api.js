e4c60ab7a89710618aa0fd6c2ff8026cf194ec6e
FrankVazquezSE <mhp3rdynot52@gmail.com>
2022-09-05 19:12:32 -0400

    Updated Weather App

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        "X-RapidAPI-Key":"",
+        "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "3d1095ef7306e2da91ea49fc068a75c1";
